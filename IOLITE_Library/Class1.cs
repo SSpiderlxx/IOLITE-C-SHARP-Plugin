a02401a8d3b9a83d36c82edf30c4b992b0329970
Leon Twemlow <leon@twemlow.co.uk>
2025-01-20 11:21:26 +0000

    editor api ported

--- IOLITE_Library/Class1.cs
+++ IOLITE_Library/Class1.cs
@@ -3,10 +3,99 @@ using System.Runtime.InteropServices;
 
 namespace IOLITE_Library
 {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct io_ref_t
+    {
+        public ushort id;
+        public byte gen;
+        public byte type;
+
+        public static io_ref_t Invalid
+        {
+            get
+            {
+                return new io_ref_t
+                {
+                    id = 0xFFFF,
+                    gen = 0xFF,
+                    type = 0xFF
+                };
+            }
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                return id != 0xFFFF && gen != 0xFF;
+            }
+        }
+
+        public bool Equals(io_ref_t other)
+        {
+            return id == other.id && gen == other.gen && type == other.type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is io_ref_t other)
+            {
+                return Equals(other);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id, gen, type);
+        }
+
+        public static bool operator ==(io_ref_t left, io_ref_t right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(io_ref_t left, io_ref_t right)
+        {
+            return !(left == right);
+        }
+    }
+
     public class Class1
     {
+
+        // Provides access to the logging subsystem
         [DllImport("iolite_api.dll")]
         public static extern void log_warning(string msg);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void log_error(string msg);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void log_info(string msg);
+
+        // Provides access to the editor
+        [DllImport("iolite_api.dll")]
+        public static extern void select_node(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t get_first_selected_node();
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t get_first_selected_entity();
+
+        // Provides access to the world subsystem
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t get_root_node();
+
+        [DllImport("iolite_api.dll")]
+        public static extern void load_world(string name);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void save_world(string name);
+
+        [DllImport("iolite_api.dll")]
+        public static extern float get_current_time_factor();
 
         public static int Run(IntPtr arg, int argLength)
         {
--- iolite_api/iolite_api.cpp
+++ iolite_api/iolite_api.cpp
@@ -6,6 +6,10 @@ const struct io_api_manager_i* io_api_manager = 0;
 
 // Declare the io_logging interface
 static const io_logging_i* io_logging = nullptr;
+// Delcate io_editor interface
+static const io_editor_i* io_editor = nullptr;
+// Delcate io_world interface
+static const io_world_i* io_world = nullptr;
 
 IO_API_EXPORT io_uint32_t IO_API_CALL get_api_version()
 {
@@ -20,6 +24,10 @@ IO_API_EXPORT io_int32_t IO_API_CALL load_plugin(const void* api_manager)
 
     // Retrieve the logging interface
     io_logging = (const io_logging_i*)io_api_manager->find_first(IO_LOGGING_API_NAME);
+    // Retrieve the editor interface
+    io_editor = (const io_editor_i*)io_api_manager->find_first(IO_EDITOR_API_NAME);
+    // Retrieve the world interface
+    io_world = (const io_world_i*)io_api_manager->find_first(IO_WORLD_API_NAME);
 
     return 0;
 }
@@ -29,7 +37,8 @@ IO_API_EXPORT void IO_API_CALL unload_plugin()
     // Clean up here
 }
 
-extern "C" __declspec(dllexport) void log_warning(const char* msg)
+// Provides access to the logging subsystem
+IO_API_EXPORT void log_warning(const char* msg)
 {
     // Ensure io_logging interface is available
     if (io_logging != nullptr)
@@ -37,3 +46,57 @@ extern "C" __declspec(dllexport) void log_warning(const char* msg)
         io_logging->log_warning(msg);
     }
 }
+
+IO_API_EXPORT void log_info(const char* msg) {
+    if (io_logging != nullptr) {
+        io_logging->log_info(msg);
+    }
+}
+
+IO_API_EXPORT void log_error(const char* msg) {
+    if (io_logging != nullptr) {
+        io_logging->log_error(msg);
+    }
+}
+
+// Provides access to the editor
+IO_API_EXPORT void select_node(io_ref_t node) {
+    if (io_editor != nullptr) {
+        io_editor->select_node(node);
+    }
+}
+
+IO_API_EXPORT io_ref_t get_first_selected_node() {
+    return io_editor->get_first_selected_node();
+}
+
+IO_API_EXPORT io_ref_t get_first_selected_entity() {
+    return io_editor->get_first_selected_entity();
+}
+
+// Provides access to the world subsystem
+IO_API_EXPORT io_ref_t get_root_node() {
+    return io_world->get_root_node();
+}
+
+IO_API_EXPORT void load_world(const char* name) {
+    io_world->load_world(name);
+}
+
+IO_API_EXPORT void save_world(const char* name) {
+    io_world->save_world(name);
+}
+
+IO_API_EXPORT io_ref_t spawn_prefab(const char* name) {
+    return io_world->spawn_prefab(name);
+}
+
+IO_API_EXPORT io_ref_t get_active_camera() {
+    return io_world->get_active_camera();
+}
+
+IO_API_CALL io_float32_t get_current_time_factor() {
+    return io_world->get_current_time_factor();
+}
+
+

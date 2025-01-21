daca5456ac2dc3afa533c12b3d0eace5a5c84288
Leon Twemlow <leon@twemlow.co.uk>
2025-01-21 02:35:28 +0000

    io_component_node intergration

--- IOLITE_Library/Class1.cs
+++ IOLITE_Library/Class1.cs
@@ -3,6 +3,62 @@ using System.Runtime.InteropServices;
 
 namespace IOLITE_Library
 {
+
+    using io_size_t = System.UInt32;
+
+    // Struct to represent a 3D vector
+    public struct io_vec3_t
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public io_vec3_t(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    // Struct to represent an axis-aligned bounding box (AABB)
+    public struct io_aabb_t
+    {
+        public io_vec3_t center;      // Center of the AABB
+        public io_vec3_t half_extent; // Half of the size of the AABB
+
+        public io_aabb_t(io_vec3_t center, io_vec3_t half_extent)
+        {
+            this.center = center;
+            this.half_extent = half_extent;
+        }
+
+        // Method to check if a point is inside the AABB
+        public bool Contains(io_vec3_t point)
+        {
+            return Math.Abs(point.x - center.x) <= half_extent.x &&
+                   Math.Abs(point.y - center.y) <= half_extent.y &&
+                   Math.Abs(point.z - center.z) <= half_extent.z;
+        }
+
+        // Method to check if this AABB intersects with another AABB
+        public bool Intersects(io_aabb_t other)
+        {
+            return Math.Abs(center.x - other.center.x) <= (half_extent.x + other.half_extent.x) &&
+                   Math.Abs(center.y - other.center.y) <= (half_extent.y + other.half_extent.y) &&
+                   Math.Abs(center.z - other.center.z) <= (half_extent.z + other.half_extent.z);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct io_quat_t
+    {
+        public float w;
+        public float x;
+        public float y;
+        public float z;
+    }
+
     [StructLayout(LayoutKind.Sequential)]
     public struct io_ref_t
     {
@@ -36,7 +92,7 @@ namespace IOLITE_Library
             return id == other.id && gen == other.gen && type == other.type;
         }
 
-        public override bool Equals(object obj)
+        public override bool Equals(object? obj)
         {
             if (obj is io_ref_t other)
             {
@@ -59,6 +115,13 @@ namespace IOLITE_Library
         {
             return !(left == right);
         }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct io_sphere_t
+    {
+        public io_vec3_t center;
+        public float radius;
     }
 
     public class Class1
@@ -95,7 +158,197 @@ namespace IOLITE_Library
         public static extern void save_world(string name);
 
         [DllImport("iolite_api.dll")]
+        public static extern io_ref_t spawn_prefab(string name);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t get_active_camera();
+
+        [DllImport("iolite_api.dll")]
         public static extern float get_current_time_factor();
+
+        // Provides access to node components
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t create_node(string name);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t create_node_with_parent(string name,  io_ref_t parent, bool ignore_parent);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void attach_node(io_ref_t parent, io_ref_t child, bool ignore_parent);
+
+        [DllImport("iolite_api.dll")]
+        public static extern int get_node_depth(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t get_node_first_child(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t get_node_next_sibling(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_ref_t get_node_prev_sibling(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_aabb_t get_node_local_bounds(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_aabb_t get_node_world_bounds(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void set_hidden(io_ref_t node, bool hidden);
+
+        [DllImport("iolite_api.dll")]
+        public static extern bool is_hidden(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void set_node_world_position(io_ref_t node, io_vec3_t pos);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_vec3_t get_node_world_position(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void set_node_size(io_ref_t node, io_vec3_t size);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_vec3_t get_node_size(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void set_node_world_size(io_ref_t node, io_vec3_t size);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_vec3_t get_node_world_size(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void set_node_orientation(io_ref_t node, io_quat_t orient);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_quat_t get_node_orientation(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void set_node_world_orientation(io_ref_t node, io_quat_t orient);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_quat_t get_node_world_orientation(io_ref_t node);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_vec3_t to_local_space(io_ref_t node, io_vec3_t pos);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_vec3_t to_local_space_direction(io_ref_t node, io_vec3_t dir);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_vec3_t to_world_space(io_ref_t node, io_vec3_t pos);
+
+        [DllImport("iolite_api.dll")]
+        public static extern io_vec3_t to_world_space_direction(io_ref_t node, io_vec3_t dir);
+
+        [DllImport("iolite_api.dll")]
+        public static extern void update_transforms(io_ref_t node);
+
+        [DllImport("iolite_api.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void update_transforms_jobified([MarshalAs(UnmanagedType.LPArray)] io_ref_t[] nodes);
+
+        [DllImport("iolite_api.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void intersect_point(
+            io_vec3_t point,
+            [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)] io_ref_t[] nodes,
+            ulong nodes_length,
+            [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)] io_ref_t[] intersecting_nodes,
+            ref ulong intersecting_nodes_length,
+            bool use_global_bounds
+        );
+
+        public static io_ref_t[] CallIntersectPoint(
+            io_vec3_t point,
+            io_ref_t[] nodes,
+            bool useGlobalBounds)
+            {
+                // Validate input
+                if (nodes == null || nodes.Length == 0)
+                {
+                    throw new ArgumentException("The nodes array must not be null or empty.");
+                }
+
+                // Prepare the output buffer with a size equal to the input nodes array
+                ulong intersectingNodesLength = (ulong)nodes.Length; // Maximum possible size
+                io_ref_t[] intersectingNodes = new io_ref_t[nodes.Length];
+
+                // Call the native function
+                intersect_point(point, nodes, (ulong)nodes.Length, intersectingNodes, ref intersectingNodesLength, useGlobalBounds);
+
+                // Resize the output array to the actual number of intersecting nodes
+                Array.Resize(ref intersectingNodes, (int)intersectingNodesLength);
+
+                // Return the intersecting nodes
+                return intersectingNodes;
+            }
+
+        [DllImport("iolite_api.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void intersect_aabb(
+            io_aabb_t aabb,
+            [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)] io_ref_t[] nodes,
+            ulong nodes_length,
+            [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)] io_ref_t[] intersecting_nodes,
+            ref ulong intersecting_nodes_length,
+            bool use_global_bounds
+        );
+
+        public static io_ref_t[] CallIntersectAABB(
+            io_aabb_t aabb,
+            io_ref_t[] nodes,
+            bool useGlobalBounds){
+                if (nodes == null || nodes.Length == 0)
+                {
+                    throw new ArgumentException("The nodes array must not be null or empty.");
+                }
+
+                // Prepare output buffer
+                ulong intersectingNodesLength = (ulong)nodes.Length; // Maximum possible size
+                io_ref_t[] intersectingNodes = new io_ref_t[nodes.Length];
+
+                // Call the native function
+                intersect_aabb(aabb, nodes, (ulong)nodes.Length, intersectingNodes, ref intersectingNodesLength, useGlobalBounds);
+
+                // Resize the output array to match the actual number of intersecting nodes
+                Array.Resize(ref intersectingNodes, (int)intersectingNodesLength);
+
+                return intersectingNodes;
+            }
+
+        [DllImport("iolite_api.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void intersect_sphere(
+            io_sphere_t sphere,
+            [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)] io_ref_t[] nodes,
+            ulong nodes_length,
+            [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)] io_ref_t[] intersecting_nodes,
+            ref ulong intersecting_nodes_length,
+            bool use_global_bounds
+        );
+
+        public static io_ref_t[] CallIntersectSphere(
+            io_sphere_t sphere,
+            io_ref_t[] nodes,
+            bool useGlobalBounds)
+                {
+                    // Validate input
+                    if (nodes == null || nodes.Length == 0)
+                    {
+                        throw new ArgumentException("The nodes array must not be null or empty.");
+                    }
+
+                    // Prepare the output buffer with a size equal to the input nodes array
+                    ulong intersectingNodesLength = (ulong)nodes.Length; // Maximum possible size
+                    io_ref_t[] intersectingNodes = new io_ref_t[nodes.Length];
+
+                    // Call the native function
+                    intersect_sphere(sphere, nodes, (ulong)nodes.Length, intersectingNodes, ref intersectingNodesLength, useGlobalBounds);
+
+                    // Resize the output array to the actual number of intersecting nodes
+                    Array.Resize(ref intersectingNodes, (int)intersectingNodesLength);
+
+                    // Return the intersecting nodes
+                    return intersectingNodes;
+                }
 
         public static int Run(IntPtr arg, int argLength)
         {
@@ -108,6 +361,7 @@ namespace IOLITE_Library
         public delegate void On_Tick_Delegate(float delta_t);
         public static void On_Tick(float delta_t){
             log_warning("On_Tick");
+            log_warning(get_active_camera().IsValid.ToString());
         }
     }
 }
--- iolite_api/iolite_api.cpp
+++ iolite_api/iolite_api.cpp
@@ -1,4 +1,7 @@
 #include "inc/iolite_api.h"
+#include "vector"
+
+using namespace std;
 
 
 //Iolite api
@@ -10,6 +13,8 @@ static const io_logging_i* io_logging = nullptr;
 static const io_editor_i* io_editor = nullptr;
 // Delcate io_world interface
 static const io_world_i* io_world = nullptr;
+// Delcate io_component_node interface
+static const io_component_node_i* io_component_node = nullptr;
 
 IO_API_EXPORT io_uint32_t IO_API_CALL get_api_version()
 {
@@ -28,6 +33,8 @@ IO_API_EXPORT io_int32_t IO_API_CALL load_plugin(const void* api_manager)
     io_editor = (const io_editor_i*)io_api_manager->find_first(IO_EDITOR_API_NAME);
     // Retrieve the world interface
     io_world = (const io_world_i*)io_api_manager->find_first(IO_WORLD_API_NAME);
+    // Retrieve the component_node interface
+    io_component_node = (const io_component_node_i*)io_api_manager->find_first(IO_COMPONENT_NODE_API_NAME);
 
     return 0;
 }
@@ -95,8 +102,223 @@ IO_API_EXPORT io_ref_t get_active_camera() {
     return io_world->get_active_camera();
 }
 
-IO_API_CALL io_float32_t get_current_time_factor() {
+IO_API_EXPORT io_float32_t get_current_time_factor() {
     return io_world->get_current_time_factor();
 }
 
+// Provides access to node components
+IO_API_EXPORT io_ref_t create_node(const char* name) {
+    return io_component_node->create(name);
+}
 
+IO_API_EXPORT io_ref_t create_node_with_parent(const char* name, io_ref_t parent, io_bool_t ignore_parent) {
+    return io_component_node->create_with_parent(name, parent, ignore_parent);
+}
+
+IO_API_EXPORT void attach_node(io_ref_t parent, io_ref_t child, io_bool_t ignore_parent) {
+    io_component_node->attach(parent, child, ignore_parent);
+}
+
+IO_API_EXPORT io_int32_t get_node_depth(io_ref_t node) {
+    return io_component_node->get_depth(node);
+}
+
+IO_API_EXPORT io_ref_t get_node_parent(io_ref_t node) {
+    return io_component_node->get_parent(node);
+}
+
+IO_API_EXPORT io_ref_t get_node_first_child(io_ref_t node) {
+    return io_component_node->get_first_child(node);
+}
+
+IO_API_EXPORT io_ref_t get_node_next_sibling(io_ref_t node) {
+    return io_component_node->get_next_sibling(node);
+}
+
+IO_API_EXPORT io_ref_t get_node_prev_sibling(io_ref_t node) {
+    return io_component_node->get_prev_sibling(node);
+}
+
+IO_API_EXPORT io_aabb_t get_node_local_bounds(io_ref_t node) {
+    return io_component_node->get_local_bounds(node);
+}
+
+IO_API_EXPORT io_aabb_t get_node_world_bounds(io_ref_t node) {
+    return io_component_node->get_world_bounds(node);
+}
+
+IO_API_EXPORT void set_hidden(io_ref_t node, io_bool_t hidden) {
+    io_component_node->set_hidden(node, hidden);
+}
+
+IO_API_EXPORT io_bool_t is_hidden(io_ref_t node) {
+    return io_component_node->is_hidden(node);
+}
+
+IO_API_EXPORT void set_node_world_position(io_ref_t node, io_vec3_t pos) {
+    io_component_node->set_world_position(node, pos);
+}
+
+IO_API_EXPORT io_vec3_t get_node_world_position(io_ref_t node) {
+    return io_component_node->get_world_position(node);
+}
+
+IO_API_EXPORT void set_node_size(io_ref_t node, io_vec3_t size) {
+    io_component_node->set_size(node, size);
+}
+
+IO_API_EXPORT io_vec3_t get_node_size(io_ref_t node) {
+    return io_component_node->get_size(node);
+}
+
+IO_API_EXPORT void set_node_world_size(io_ref_t node, io_vec3_t size) {
+    io_component_node->set_world_size(node, size);
+}
+
+IO_API_EXPORT io_vec3_t get_node_world_size(io_ref_t node) {
+    return io_component_node->get_world_size(node);
+}
+
+IO_API_EXPORT void set_node_orientation(io_ref_t node, io_quat_t orient) {
+    io_component_node->set_orientation(node, orient);
+}
+
+IO_API_EXPORT io_quat_t get_node_orientation(io_ref_t node) {
+    return io_component_node->get_orientation(node);
+}
+
+IO_API_EXPORT void set_node_world_orientation(io_ref_t node, io_quat_t orient) {
+    io_component_node->set_world_orientation(node, orient);
+}
+
+IO_API_EXPORT io_quat_t get_node_world_orientation(io_ref_t node) {
+    return io_component_node->get_world_orientation(node);
+}
+
+IO_API_EXPORT io_vec3_t to_local_space(io_ref_t node, io_vec3_t pos) {
+    return io_component_node->to_local_space(node, pos);
+}
+
+IO_API_EXPORT io_vec3_t to_local_space_direction(io_ref_t node, io_vec3_t dir) {
+    return io_component_node->to_local_space_direction(node, dir);
+}
+
+IO_API_EXPORT io_vec3_t to_world_space(io_ref_t node, io_vec3_t pos) {
+    return io_component_node->to_world_space(node, pos);
+}
+
+IO_API_EXPORT io_vec3_t to_world_space_direction(io_ref_t node, io_vec3_t dir) {
+    return io_component_node->to_world_space_direction(node, dir);
+}
+
+IO_API_EXPORT void update_transforms(io_ref_t node) {
+    io_component_node->update_transforms(node);
+}
+
+IO_API_EXPORT void update_transforms_jobified(const std::vector<io_ref_t>& nodes) {
+    if (nodes.empty()) {
+        io_logging->log_error("The nodes vector must not be empty.");
+    }
+
+    const io_ref_t* nodes_ptr = nodes.data();
+    io_size_t nodes_length = static_cast<io_size_t>(nodes.size());
+    io_component_node->update_transforms_jobified(nodes_ptr, nodes_length);
+}
+
+IO_API_EXPORT void intersect_point(io_vec3_t point,
+    const std::vector<io_ref_t>& nodes,
+    std::vector<io_ref_t>& intersecting_nodes,
+    io_bool_t use_global_bounds) {
+    // Check that the input vector `nodes` is not empty
+    if (nodes.empty()) {
+        io_logging->log_error("The nodes vector must not be empty.");
+    }
+
+    // Prepare the inputs
+    const io_ref_t* nodes_ptr = nodes.data();
+    io_size_t nodes_length = static_cast<io_size_t>(nodes.size());
+
+    // Prepare the output for intersecting nodes
+    io_size_t intersecting_nodes_length = 0; // This will be updated by the intersect_point function
+    intersecting_nodes.clear(); // Ensure the output vector is empty
+
+    // Reserve enough space in the output vector to accommodate the results
+    intersecting_nodes.resize(nodes.size());
+
+    // Call the intersect_point function
+    io_component_node->intersect_point(point,
+        nodes_ptr,
+        nodes_length,
+        intersecting_nodes.data(),
+        &intersecting_nodes_length,
+        use_global_bounds);
+
+    // Resize the output vector to match the actual number of intersecting nodes
+    intersecting_nodes.resize(intersecting_nodes_length);
+}
+
+IO_API_EXPORT void intersect_aabb(io_aabb_t aabb,
+    const std::vector<io_ref_t>& nodes,
+    std::vector<io_ref_t>& intersecting_nodes,
+    io_bool_t use_global_bounds) {
+    // Check that the input vector `nodes` is not empty
+    if (nodes.empty()) {
+        io_logging->log_error("The nodes vector must not be empty.");
+        return;
+    }
+
+    // Prepare the inputs
+    const io_ref_t* nodes_ptr = nodes.data();
+    io_size_t nodes_length = static_cast<io_size_t>(nodes.size());
+
+    // Prepare the output for intersecting nodes
+    io_size_t intersecting_nodes_length = 0; // This will be updated by the intersect_aabb function
+    intersecting_nodes.clear();              // Ensure the output vector is empty
+
+    // Reserve enough space in the output vector to accommodate the results
+    intersecting_nodes.resize(nodes.size());
+
+    // Call the intersect_aabb function
+    io_component_node->intersect_aabb(aabb,
+        nodes_ptr,
+        nodes_length,
+        intersecting_nodes.data(),
+        &intersecting_nodes_length,
+        use_global_bounds);
+
+    // Resize the output vector to match the actual number of intersecting nodes
+    intersecting_nodes.resize(intersecting_nodes_length);
+}
+
+IO_API_EXPORT void intersect_sphere(io_sphere_t sphere,
+    const std::vector<io_ref_t>& nodes,
+    std::vector<io_ref_t>& intersecting_nodes,
+    io_bool_t use_global_bounds) {
+    // Check that the input vector `nodes` is not empty
+    if (nodes.empty()) {
+        io_logging->log_error("The nodes vector must not be empty.");
+        return;
+    }
+
+    // Prepare the inputs
+    const io_ref_t* nodes_ptr = nodes.data();
+    io_size_t nodes_length = static_cast<io_size_t>(nodes.size());
+
+    // Prepare the output for intersecting nodes
+    io_size_t intersecting_nodes_length = 0; // This will be updated by the intersect_sphere function
+    intersecting_nodes.clear();              // Ensure the output vector is empty
+
+    // Reserve enough space in the output vector to accommodate the results
+    intersecting_nodes.resize(nodes.size());
+
+    // Call the intersect_sphere function
+    io_component_node->intersect_sphere(sphere,
+        nodes_ptr,
+        nodes_length,
+        intersecting_nodes.data(),
+        &intersecting_nodes_length,
+        use_global_bounds);
+
+    // Resize the output vector to match the actual number of intersecting nodes
+    intersecting_nodes.resize(intersecting_nodes_length);
+}
